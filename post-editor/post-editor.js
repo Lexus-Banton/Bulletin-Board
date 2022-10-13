e5df60ce03360e1512511a6fa54cef1dcb15e81d
Lexus Banton <lexusbanton.kr@gmail.com>
2022-10-12 20:47:29 -0700

    adds post form design

--- post-editor/post-editor.js
+++ post-editor/post-editor.js
@@ -1,5 +1,15 @@
 /* Imports */
 import '../auth/user.js';
+import { createPost } from '../fetch-utils.js';
 
 /* Dom Elements */
 const postForm = document.getElementById('post-form');
+
+/* state*/
+
+let error = null;
+let post = null;
+
+/* Events*/
+
+window.addEventListener('load', async () => {});
